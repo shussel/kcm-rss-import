1a6604870d6f1facfe31be1c12398402edc3bc47
Shane Hussel <shussel@gmail.com>
2016-08-01 22:26:01 -0400

    simplify structure

--- kcm-rss-import.php
+++ kcm-rss-import.php
@@ -32,7 +32,6 @@ if ( ! defined( 'WPINC' ) ) {
 
 /**
  * The code that runs during plugin activation.
- * This action is documented in includes/class-kcm-rss-import-activator.php
  */
 function activate_kcm_rss_import() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-kcm-rss-import-activator.php';
@@ -41,7 +40,6 @@ function activate_kcm_rss_import() {
 
 /**
  * The code that runs during plugin deactivation.
- * This action is documented in includes/class-kcm-rss-import-deactivator.php
  */
 function deactivate_kcm_rss_import() {
 	require_once plugin_dir_path( __FILE__ ) . 'includes/class-kcm-rss-import-deactivator.php';
@@ -52,24 +50,207 @@ register_activation_hook( __FILE__, 'activate_kcm_rss_import' );
 register_deactivation_hook( __FILE__, 'deactivate_kcm_rss_import' );
 
 /**
- * The core plugin class that is used to define internationalization,
- * admin-specific hooks, and public-facing site hooks.
+ * Main plugin class
  */
-require plugin_dir_path( __FILE__ ) . 'includes/class-kcm-rss-import.php';
+class Kcm_Rss_Import {
 
-/**
- * Begins execution of the plugin.
- *
- * Since everything within the plugin is registered via hooks,
- * then kicking off the plugin from this point in the file does
- * not affect the page life cycle.
- *
- * @since    1.0.0
- */
-function run_kcm_rss_import() {
+	/**
+	 * The unique identifier of this plugin.
+	 *
+	 * @since    1.0.0
+	 * @access   protected
+	 * @var      string    $plugin_name    The string used to uniquely identify this plugin.
+	 */
+	protected $plugin_name = 'kcm_rss_import';
 
-	$plugin = new Kcm_Rss_Import();
-	$plugin->run();
+	/**
+	 * The options name to be used in this plugin
+	 *
+	 * @since  	1.0.0
+	 * @access 	private
+	 * @var  	string 		$option_name 	Option name of this plugin
+	 */
+	private $option_name = 'kcm_rss_import';
 
+	/**
+	 * Static property to hold our singleton instance
+	 *
+	 */
+	static $instance = false;
+
+	/**
+	 * This is our constructor
+	 *
+	 * @return void
+	 */
+	private function __construct() {
+		
+		add_action( 'admin_init', array( $this, 'admin_init' ) );
+		add_action( 'admin_menu', array( $this, 'add_options_page' ) );
+
+		// Add settings link to plugin
+		$plugin_basename = plugin_basename( plugin_dir_path( __DIR__ ) . $this->plugin_name . '.php' );
+		add_filter( 'plugin_action_links_' . $plugin_basename, array( $this, 'add_action_links' ) );
+	}
+
+	/**
+	 * If an instance exists, this returns it.  If not, it creates one and
+	 * returns it.
+	 *
+	 * @return Kcm_Rss_Import
+	 */
+	public static function getInstance() {
+		if ( !self::$instance )
+			self::$instance = new self;
+		return self::$instance;
+	}
+
+	/**
+	 * Register settings for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function admin_init() {
+
+		register_setting( $this->plugin_name, $this->option_name . '_member_id' );
+		register_setting( $this->plugin_name, $this->option_name . '_category' );
+		
+		// add sections
+		add_settings_section(
+			$this->option_name . '_account',
+			__( 'Account Info', 'kcm-rss-import' ),
+			array( $this, 'section_callback' ),
+			$this->plugin_name
+		);
+
+		add_settings_section(
+			$this->option_name . '_import',
+			__( 'Import Settings', 'kcm-rss-import' ),
+			array( $this, 'section_callback' ),
+			$this->plugin_name
+		);
+
+		// Add Member ID setting
+		add_settings_field(
+			$this->option_name . '_member_id',
+			__( 'KCM Member ID', 'kcm-rss-import' ),
+			array( $this, $this->option_name . '_member_id_cb' ),
+			$this->plugin_name,
+			$this->option_name . '_account',
+			array( 'label_for' => $this->option_name . '_member_id' )
+		);	
+		
+		// Add Member ID setting
+		add_settings_field(
+			$this->option_name . '_category',
+			__( 'Add Posts to Category', 'kcm-rss-import' ),
+			array( $this, $this->option_name . '_category_cb' ),
+			$this->plugin_name,
+			$this->option_name . '_import',
+			array( 'label_for' => $this->option_name . '_category' )
+		);
+	}
+
+	/**
+	 * Add settings action link to the plugins page.
+	 *
+	 * @since    1.0.0
+	 */
+	 
+	public function add_action_links( $links ) {
+	   $settings_link = array(
+		'<a href="' . admin_url( 'options-general.php?page=' . $this->plugin_name ) . '">' . __('Settings', $this->plugin_name) . '</a>',
+	   );
+	   return array_merge(  $settings_link, $links );
+	}
+
+	/**
+	 * Add an options page under the Settings submenu
+	 *
+	 * @since  1.0.0
+	 */
+	public function add_options_page() {
+	
+		$this->plugin_screen_hook_suffix = add_options_page(
+			__( 'KCM RSS Import Settings', 'kcm-rss-import' ),
+			__( 'KCM RSS Import', 'kcm-rss-import' ),
+			'manage_options',
+			$this->plugin_name,
+			array( $this, 'display_options_page' )
+		);
+	
+	}
+
+	/**
+	 * Render the options page for plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function display_options_page() {
+		include_once 'partials/kcm-rss-import-admin-display.php';
+	}
+
+	/**
+	 * Display section info
+	 *
+	 * @since  1.0.0
+	 */
+	public function section_callback( $arguments ) {
+		switch( $arguments['id'] ){
+			case $this->option_name . '_account':
+				echo 'Enter your 39 digit member id from your RSS link to activate the import.';
+				break;
+			case $this->option_name . '_import':
+				echo 'Select the category into which your posts are imported.';
+				break;
+		}
+	}
+
+		/**
+	 * Render the member id input for this plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function kcm_rss_import_member_id_cb() {
+		$member_id = get_option( $this->option_name . '_member_id' );
+		echo '<input type="text" size="40" name="' . $this->option_name . '_member_id' . '" id="' . $this->option_name . '_member_id' . '" value="' . $member_id . '"><span class="description"> Enter everything after"a=" in your RSS URL</span>';
+	}
+
+	/**
+	 * Render the category selector for this plugin
+	 *
+	 * @since  1.0.0
+	 */
+	public function kcm_rss_import_category_cb() {
+
+		$category = get_option( $this->option_name . '_category' );
+
+		$args = array(
+			'show_option_all'    => '',
+			'show_option_none'   => '',
+			'option_none_value'  => '-1',
+			'orderby'            => 'ID',
+			'order'              => 'ASC',
+			'show_count'         => 0,
+			'hide_empty'         => 0,
+			'child_of'           => 0,
+			'exclude'            => '',
+			'include'            => '',
+			'echo'               => 1,
+			'selected'           => $category,
+			'hierarchical'       => 0,
+			'name'               => 'kcm_rss_import_category',
+			'id'                 => '',
+			'class'              => 'postform',
+			'depth'              => 0,
+			'tab_index'          => 0,
+			'taxonomy'           => 'category',
+			'hide_if_empty'      => false,
+			'value_field'	     => 'term_id',
+		);
+		wp_dropdown_categories( $args );
+	}
 }
-run_kcm_rss_import();
+
+// Instantiate class
+$Kcm_Rss_Import = Kcm_Rss_Import::getInstance();
